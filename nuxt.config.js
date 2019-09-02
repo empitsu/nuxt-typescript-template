6401c43f327aa8cd550cae53c55d925977bd3180
empitsu <yoshie8080@gmail.com>
2019-09-02 16:39:26 +0900

    feat: add typescript to Nuxt.js 2.9

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,7 +33,9 @@ export default {
    */
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module'
+    '@nuxtjs/eslint-module',
+    // Doc: https://typescript.nuxtjs.org/guide/setup.html
+    '@nuxt/typescript-build'
   ],
   /*
    ** Nuxt.js modules
@@ -46,6 +48,6 @@ export default {
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {}
+    // extend(config, ctx) {}
   }
 }
